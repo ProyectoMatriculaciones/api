fedec8e2179b8ee1d860fd92cebc1716571ac085
Pablo Martinez <pmirazabal@gmail.com>
2021-03-17 20:13:23 +0100

    spec 13 + verificacion de tokens por enrutamientos

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 var MongoClient = require('mongodb').MongoClient;
 
 //example --> var mongoUrl = "mongodb+srv://<username>:<password>@mongoexample.wf7zu.mongodb.net/mongoexample?retryWrites=true&w=majority"
-var mongoUrl = process.env.MONGOURL
+var mongoUrl = process.env.MONGOURL || "mongodb+srv://Dani:Monsterhunter3@firstcluster.4htxv.mongodb.net/myFirstDatabase?retryWrites=true&w=majority"
 
 
 var bodyParser = require('body-parser')
@@ -15,6 +15,7 @@ const port = process.env.PORT || 3000
 const mongoDb = "MatriculationDB"
 const collectionAlum = "Alumn"
 const collectionAdmin = "Admin"
+const collectionGrade = "FormativeDegree"
 
 const jwt = require('jsonwebtoken')
 
@@ -22,6 +23,39 @@ const alphaNumeric = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUV
 
 
 const config = require('./configs/configs');
+
+// Enrutamiento para proteger endpoints con autenticacion por token
+const protectedRoute = express.Router(); 
+protectedRoute.use((req, res, next) => {
+    const token = req.headers['access-token']; 
+    if (token) {
+      jwt.verify(token, app.get('jwtKey'), (err, decoded) => {      
+        if (err) {
+          res.status(400).send({error: "El token informado no es valido" });    
+        } else {
+          req.decoded = decoded;    
+          next();
+        }
+      });
+    } else {
+      res.status(400).send({error: "Token no informada en el header access-token"});
+    }
+ });
+
+// Segundo enrutamiento para rutas protegidas con permisos de administrador
+const checkAdminToken = express.Router(); 
+checkAdminToken.use((req, res, next) => {
+    if (req.decoded.admin != undefined && req.decoded.admin == true)
+    {
+    	next();
+    }
+    else
+    {
+    	res.status(400).send({error: "No permitido"});
+    }
+ });
+
+
 
 app.set('jwtKey', config.jwtKey)
 
@@ -38,7 +72,9 @@ app.use((req, res, next) => {
 });
 
 
-
+// ---------------------------------------------------------
+// post /login/alumn
+// ---------------------------------------------------------
 
 app.post('/login/alumn', (req, res) => {
 	var username = req.body.username;
@@ -72,13 +108,13 @@ function checkAlumnLogin(usr, pass, res)
 				res.status(400).send({"status":"KO","statusData":"Los datos de login son incorrectos"})
 			}			
 			db.close();
-		});		
+		});			
 	});
 }
 
 function alumnLoginCallback(result, res, db, dbo) 
 {
-	var token = jwt.sign({id : result._id}, app.get('jwtKey'), {expiresIn: 1440});
+	var token = jwt.sign({id : result._id, admin : false}, app.get('jwtKey'), {expiresIn: 1440});
 	var query = {_id : result._id};
 	var newValues = { $set: {sessionToken: token} };
 	dbo.collection(collectionAlum).updateOne(query, newValues, function(err, updResult){
@@ -88,6 +124,12 @@ function alumnLoginCallback(result, res, db, dbo)
 	});	
 }
 
+
+
+
+// ---------------------------------------------------------
+// post /login/admin
+// ---------------------------------------------------------
 app.post('/login/admin', (req, res) => {
 	var username = req.body.username;
 	var password = req.body.password;
@@ -126,7 +168,7 @@ function checkAdminLogin(usr, pass, res)
 
 function adminLoginCallback(result, res, db, dbo) 
 {
-	var token = jwt.sign({id : result._id}, app.get('jwtKey'), {expiresIn: 1440});
+	var token = jwt.sign({id : result._id, admin : true}, app.get('jwtKey'), {expiresIn: 1440});
 	var query = {_id : result._id};
 	var newValues = { $set: {sessionToken: token} };
 	dbo.collection(collectionAdmin).updateOne(query, newValues, function(err, updResult){
@@ -138,13 +180,65 @@ function adminLoginCallback(result, res, db, dbo)
 
 
 
-function randomString(length, chars) {
-    var result = '';
-    for (var i = length; i > 0; --i) result += chars[Math.floor(Math.random() * chars.length)];
-    return result;
-}	
+
+// ---------------------------------------------------------
+// get /get/allGrades
+// ---------------------------------------------------------
+app.get('/get/allGrades', protectedRoute, checkAdminToken, (req, res) => {
+
+	MongoClient.connect(mongoUrl, function(err, db) {
+		if (err) throw err;
+		var dbo = db.db(mongoDb);
+		dbo.collection(collectionGrade).find({}).project({careerCode : 1, careerName : 1}).toArray(function(err, result) {
+			if (err) throw err;						
+			res.status(200).send(result);
+			db.close();
+		});
+		
+	});
+	
+})
 
 
+
+
+// ---------------------------------------------------------
+// get /get/allGrades
+// ---------------------------------------------------------
+app.get('/get/grade', (req, res) => {
+	var qCareerCode = req.query.careerCode
+	if (qCareerCode == undefined){
+		res.status(400).send("No se ha informado de un careerCode en la query")
+	}
+
+	MongoClient.connect(mongoUrl, function(err, db) {
+		if (err) throw err;
+		var dbo = db.db(mongoDb);
+		dbo.collection(collectionGrade).findOne({careerCode : qCareerCode}, function(err, result) {
+			if (err) throw err;		
+			if (result != null)
+			{
+				res.status(200).send(result);
+			}	
+			else
+			{
+				res.status(400).send("No se ha encontrado ningun ciclo con ese careerCode");
+			}			
+			
+			db.close();
+		});
+		
+	});
+	
+})
+
+
+
+
+
+// ---------------------------------------------------------
+// listen port
+// ---------------------------------------------------------
 app.listen(port, () => {
   console.log(`app running, listening at ${port}`)
 })
