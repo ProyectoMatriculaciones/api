1505ae909b8fc1b6904af128385e7d894e790865
Pablo <pmirazabal@gmail.com>
2021-04-10 12:41:02 +0200

    allow header token

--- index.js
+++ index.js
@@ -34,7 +34,7 @@ const jwtKey = process.env.JWTKEY
 // Enrutamiento para todas las peticiones que lleguen, prepara el header de la response para evitar errores
 app.use((req, res, next) => {
     res.header('Access-Control-Allow-Origin', '*');
-    res.header('Access-Control-Allow-Headers', 'Content-Type');
+    res.header('Access-Control-Allow-Headers', 'Content-Type, access-token');
     res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
     res.header('Allow', 'GET, POST, OPTIONS, PUT, DELETE');
     next();
