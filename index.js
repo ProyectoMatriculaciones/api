225a4c3cf05fb197cc7a855c743a48fe3b927378
Pablo Martinez <pmirazabal@gmail.com>
2021-04-19 16:08:56 +0200

    get document, update validate

--- index.js
+++ index.js
@@ -717,71 +717,83 @@ app.post('/upload/documentsFile', protectedRoute, (req, res) => {
 	var qEmail = req.body.email;
 	var qProfileName = req.body.profileName;
 	var qDocumentName = req.body.documentName;
-	var file = req.files.file;
-	var fileName = file.name; 
-	// get email without symbols
-	var emailNoSymbols = qEmail.replace('.','').replace('@','');		
+	var file = undefined;
+	var fileName = undefined; 
 
-	MongoClient.connect(mongoUrl, function(err, db) {
-		if (err) {
-			res.status(400).send({"error": "Error inesperado en el servidor" });
-			console.log("ERROR MONGO: " + err);
-			return;
-		}	
-		var dbo = db.db(mongoDb);			
-		dbo.collection(collectionAlum).findOne({email : qEmail, 'selectedDocumentsProfile.name' : qProfileName, 'selectedDocumentsProfile.arrayDoc.documentName' : qDocumentName}, function(err, result) {
+	if (qEmail != undefined && qProfileName != undefined && qDocumentName != undefined && req.files != undefined)
+	{
+		file = req.files.file;
+		fileName = file.name;
+		// get email without symbols
+		var emailNoSymbols = qEmail.replace('.','').replace('@','');		
+
+		MongoClient.connect(mongoUrl, function(err, db) {
 			if (err) {
 				res.status(400).send({"error": "Error inesperado en el servidor" });
 				console.log("ERROR MONGO: " + err);
-			}		
-			else if (result != null)
-			{
-				// create dirs if not exists (baseDir, email, profile, document)
-				var dirUploads = appDir + '/uploads';
-				createDirIfNotExists(dirUploads)
-
-				var dirAlumn = dirUploads + '/' + emailNoSymbols;
-				createDirIfNotExists(dirAlumn);
-
-				var dirAlumnProfile = dirAlumn + '/' + qProfileName;
-				createDirIfNotExists(dirAlumnProfile);
-
-				var dirAlumnProfileDocument = dirAlumnProfile + '/' + qDocumentName;
-				createDirIfNotExists(dirAlumnProfileDocument);
-
-				// save file and response
-				file.mv(dirAlumnProfileDocument + '/' + fileName, function(err){
-					if (err){
-						res.status(400).send({"error":"no se ha podido subir el fichero"})
-					}
-				});
-
-				// update filePath 
-				var query = {email : qEmail, 'selectedDocumentsProfile.name' : qProfileName, 'selectedDocumentsProfile.arrayDoc.documentName' : qDocumentName};
-				var newValues = { $set: {'selectedDocumentsProfile.arrayDoc.$.filePath': dirAlumnProfileDocument + '/' + fileName} };
-				dbo.collection(collectionAlum).updateOne(query, newValues, function(err, updResult){
-					if (err) {
-						res.status(400).send({"error": "Error inesperado en el servidor" });
-						console.log("ERROR MONGO: " + err);
-					}
-					else if (updResult)
-					{
-						res.status(200).send({"ok":"fichero subido correctamente"});
-					}
-					else
-					{
-						res.status(400).send({"error":"no se ha podido actualizar el campo de ruta en la base de datos"});
-					}				
-					db.close();
-				});	
+				return;
 			}	
-			else
-			{
-				res.status(400).send({"error":"No se ha encontrado ningun alumno con ese email, perfil y documento"});
-			}		
-			db.close();
-	});		
-	});
+			var dbo = db.db(mongoDb);			
+			dbo.collection(collectionAlum).findOne({email : qEmail, 'selectedDocumentsProfile.name' : qProfileName, 'selectedDocumentsProfile.arrayDoc.documentName' : qDocumentName}, function(err, result) {
+				if (err) {
+					res.status(400).send({"error": "Error inesperado en el servidor" });
+					console.log("ERROR MONGO: " + err);
+				}		
+				else if (result != null)
+				{
+					// create dirs if not exists (baseDir, email, profile, document)
+					var dirUploads = appDir + '/uploads';
+					createDirIfNotExists(dirUploads)
+
+					var dirAlumn = dirUploads + '/' + emailNoSymbols;
+					createDirIfNotExists(dirAlumn);
+
+					var dirAlumnProfile = dirAlumn + '/' + qProfileName;
+					createDirIfNotExists(dirAlumnProfile);
+
+					var dirAlumnProfileDocument = dirAlumnProfile + '/' + qDocumentName;
+					createEmptyDirRemoveIfExists(dirAlumnProfileDocument);
+
+					// save file 
+					file.mv(dirAlumnProfileDocument + '/' + fileName, function(err){
+						if (err){
+							res.status(400).send({"error":"no se ha podido subir el fichero"})
+						}
+					});
+
+					// update filePath and response
+					var query = {email : qEmail, 'selectedDocumentsProfile.name' : qProfileName, 'selectedDocumentsProfile.arrayDoc.documentName' : qDocumentName};
+					var newValues = { $set: {'selectedDocumentsProfile.arrayDoc.$.filePath': dirAlumnProfileDocument + '/' + fileName} };
+					dbo.collection(collectionAlum).updateOne(query, newValues, function(err, updResult){
+						if (err) {
+							res.status(400).send({"error": "Error inesperado en el servidor" });
+							console.log("ERROR MONGO: " + err);
+						}
+						else if (updResult)
+						{
+							res.status(200).send({"ok":"fichero subido correctamente"});
+						}
+						else
+						{
+							res.status(400).send({"error":"no se ha podido actualizar el campo de ruta en la base de datos"});
+						}				
+						db.close();
+					});	
+				}	
+				else
+				{
+					res.status(400).send({"error":"No se ha encontrado ningun alumno con ese email, perfil y documento"});
+				}		
+				db.close();
+			});		
+		});
+	}
+	else
+	{
+		res.status(400).send({"error":"No se ha informado de algun campo obligatorio"});
+	}
+
+	
 
 	
 })
@@ -792,6 +804,133 @@ function createDirIfNotExists(dir)
 		fs.mkdirSync(dir);
 	}
 }
+
+function createEmptyDirRemoveIfExists(dir)
+{
+	if (!fs.existsSync(dir)){
+		fs.mkdirSync(dir);
+	}
+	else
+	{
+		fs.rmdirSync(dir, { recursive: true });
+		fs.mkdirSync(dir);
+	}
+}
+
+
+
+// ---------------------------------------------------------
+// post /get/document
+// ---------------------------------------------------------
+app.post('/get/document', protectedRoute, checkAdminToken, (req, res) => {
+	var qUsername = req.body.email
+	var qProfileName = req.body.profileName
+	var qDocumentName = req.body.documentName
+	if (qUsername != undefined && qProfileName != undefined && qDocumentName != undefined)
+	{
+		var query = {email : qUsername, 'selectedDocumentsProfile.name' : qProfileName, 'selectedDocumentsProfile.arrayDoc.documentName' : qDocumentName};
+		var project = {projection: {'selectedDocumentsProfile.arrayDoc.$.filePath' : 1} };
+		MongoClient.connect(mongoUrl, function(err, db) {
+			if (err) {
+				res.status(400).send({"error": "Error inesperado en el servidor" });
+				console.log("ERROR MONGO: " + err);
+			}	
+			else
+			{
+				var dbo = db.db(mongoDb);
+				dbo.collection(collectionAlum).findOne(query, project, function(err, result) {
+					if (err) {
+						res.status(400).send({"error": "Error inesperado en el servidor" });
+						console.log("ERROR MONGO: " + err);
+					}		
+					else if (result != null)
+					{
+						console.log(result);
+						let buff = fs.readFileSync(result.filePath);
+						let base64data = buff.toString('base64');
+
+						res.status(200).send({"filePath" : result.filePath, "data" : base64data});
+					}	
+					else
+					{
+						res.status(400).send({"error":"No se ha encontrado ningun resultado"});
+					}			
+					
+					db.close();
+				});
+			}						
+		});
+	}
+	else
+	{
+		res.status(400).send({"error":"No se ha informado de algun campo obligatorio"});	
+	}
+	
+	
+})
+
+
+
+
+//---------------------------------------------------------
+// post /update/validateFile
+// ---------------------------------------------------------
+app.post('/update/validateFile', protectedRoute, checkAdminToken, (req, res) => {	
+	// req info
+	var qEmail = req.body.email;
+	var qProfileName = req.body.profileName;
+	var qDocumentName = req.body.documentName; 
+
+	if (qEmail != undefined && qProfileName != undefined && qDocumentName != undefined)
+	{
+		MongoClient.connect(mongoUrl, function(err, db) {
+			if (err) {
+				res.status(400).send({"error": "Error inesperado en el servidor" });
+				console.log("ERROR MONGO: " + err);
+				return;
+			}	
+			var dbo = db.db(mongoDb);			
+			dbo.collection(collectionAlum).findOne({email : qEmail, 'selectedDocumentsProfile.name' : qProfileName, 'selectedDocumentsProfile.arrayDoc.documentName' : qDocumentName}, function(err, result) {
+				if (err) {
+					res.status(400).send({"error": "Error inesperado en el servidor" });
+					console.log("ERROR MONGO: " + err);
+				}		
+				else if (result != null)
+				{
+					// update validate and response
+					var query = {email : qEmail, 'selectedDocumentsProfile.name' : qProfileName, 'selectedDocumentsProfile.arrayDoc.documentName' : qDocumentName};
+					var newValues = { $set: {'selectedDocumentsProfile.arrayDoc.$.validate': "OK"} };
+					dbo.collection(collectionAlum).updateOne(query, newValues, function(err, updResult){
+						if (err) {
+							res.status(400).send({"error": "Error inesperado en el servidor" });
+							console.log("ERROR MONGO: " + err);
+						}
+						else if (updResult)
+						{
+							res.status(200).send({"ok":"fichero validado correctamente"});
+						}
+						else
+						{
+							res.status(400).send({"error":"no se ha podido actualizar el campo de validacion en la base de datos"});
+						}				
+						db.close();
+					});	
+				}	
+				else
+				{
+					res.status(400).send({"error":"No se ha encontrado ningun alumno con ese email, perfil y documento"});
+				}		
+				db.close();
+			});		
+		});
+	}
+	else
+	{
+		res.status(400).send({"error":"No se ha informado de algun campo obligatorio"});
+	}
+})
+
+
 
 
 
